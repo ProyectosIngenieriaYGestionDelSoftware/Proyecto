f312e416c1d92c0d8c65fe1609dfec7e9c41fc55
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2024-03-21 15:31:15 +0000

    Token system created

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const body_parser = require('body-parser')
 const PORT = process.env.PORT || 3000; 
-const api = "/api/"
+const api = "/api/";
 
 const UserRegister = require("./auth/UserRegister");
 const UserAuth = require("./auth/UserAuth");
+const AuthService = require("./auth/AuthService");
 
-app.get(api, (req, res) => {
+app.use(body_parser.json())
+
+app.get(api, AuthService.verifyToken , (req, res) => {
     res.json({'message': 'ok'});
 });
 
--- backend/auth/UserAuth.js
+++ backend/auth/UserAuth.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("./AuthService");
 
-router.post("/login", async function(req, res, next){
-    res.json({'message' : 'Login direction'});
+router.post("/login", async function (req, res, next) {
+    console.log(req.body);
+    const { username, password } = req.body;
+    // TODO: Verificar las credenciales con la base de datos
+    if (username === "usuarioDB" && password === "contraseñaDB") {
+        const token = auth.generateToken(username, ["1","2","3"]);
+        res.json({ token });
+    } else {
+        res.status(401).json({ message: "Inorrect Credentials" });
+  }
 });
 
-router.post("/logout", async function(req, res, next){
-    res.json({'message' : "Logout direction"});
+router.post("/logout", auth.verifyToken, async function(req, res, next){
+    auth.invalidateToken(req.headers.authorization);
 });
 
 module.exports = router
