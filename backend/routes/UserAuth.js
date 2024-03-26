66a843975681954f4dad10c43de58ee10e95c92a
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2024-03-26 11:31:42 +0000

    Added password encryption method

--- backend/routes/UserAuth.js
+++ backend/routes/UserAuth.js
@@ -2,17 +2,26 @@ const express = require('express');
 const router = express.Router();
 const auth = require("../auth/AuthService");
 const verifyToken = require("../middlewares/VerifyTokens");
+const bcrypt = require('bcrypt');
 
 router.post("/login", async function (req, res, next) {
     console.log(req.body);
     const { username, password } = req.body;
     // TODO: Verificar las credenciales con la base de datos
-    if (username === "usuarioDB" && password === "contraseñaDB") {
-        const token = auth.generateToken(username, ["1","2","3"]);
-        res.json({ token });
-    } else {
-        res.status(401).json({ message: "Inorrect Credentials" });
-    }
+    let dbPassword = "";
+    bcrypt.compare(password, dbPassword,(err, same) => {
+        if(err){
+            res.status(404).json({'message' : "Error during user authentication", "Error" : err});
+            return;
+        }
+        if(same){
+            const token = auth.generateToken(username, ["1","2","3"]);
+            res.status(200).json({ token });
+        }else{
+            res.status(401).json({ message: "Inorrect Credentials" });
+        }
+    })
+
 });
 
 router.post("/logout", verifyToken, async function(req, res, next){
