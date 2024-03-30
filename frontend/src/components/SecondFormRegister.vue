417f6933a0192333c03ec18f8720aeab092993d6
jpere <jpereiro1@gmail.com>
2024-03-30 11:17:22 +0000

    Starting the second form of registration

---
+++ frontend/src/components/SecondFormRegister.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-form @submit.prevent="onSubmit">
+
+    <section class="sectionTypeUser">
+
+      <v-card link @click="selectItem(firstOption)" :class="{ 'outlined': clientSelected }">
+        <v-card-title>
+            <h1 class="mb-5 text-h5">Client</h1>
+        </v-card-title>
+        <v-img  :width="150" src="../assets/clientImageRegister.png"></v-img>
+        
+      </v-card>
+
+      <v-card link @click="selectItem(secondOption)" :class="{ 'outlined': businessSelected }">
+          <v-card-title>
+              <h1 class="mb-5 text-h5">Bussiness</h1>
+          </v-card-title>
+          <v-img :width="150" src="../assets/companyImageRegister.png"></v-img>
+      </v-card>
+
+    </section>
+
+    <v-btn class="mt-2"  block type="submit">Register</v-btn>
+  </v-form>
+
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    likes: Number
+  },
+  methods:{
+    onSubmit(){
+      if(this.businessSelected||this.clientSelected) console.log("asdf");
+    },
+    selectItem(item){
+      if(item==this.firstOption) {
+        if(this.businessSelected) this.businessSelected = false;
+        this.clientSelected = !this.clientSelected
+      }else{
+        if(this.clientSelected) this.clientSelected = false;
+        this.businessSelected =  !this.businessSelected;
+      }
+    }
+  },
+  data(){
+    return {
+      clientSelected:false,
+      businessSelected:false,
+      firstOption:"Client",
+      secondOption:"Business"
+    }
+  }
+
+}
+</script>
+
+<style>
+v-form{
+  display: flex;
+  flex-direction: column;
+}
+.sectionTypeUser{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  justify-content: space-around;
+}
+.outlined {
+  border: 1px solid rgba(0, 0, 0, 1);
+}
+</style>
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,60 +1,71 @@
 <template>
   <v-card class="mx-auto card">
-      
-    <v-form ref="form" novalidate @submit.prevent="onSubmit">
+    <v-card-title>
       <h1 class="mb-5 text-h3">Sign Up</h1>
-      <v-divider class="mb-5"></v-divider>
-      <v-text-field
-        class="mb-1"
-        v-model="username.value.value"
-        :error-messages="username.errorMessage.value"
-        type="text"
-        label="Username"
-        append-inner-icon="mdi-account"
-        required
-      ></v-text-field>
-      <v-text-field
-        class="mb-1"
-        v-model="email.value.value"
-        :error-messages="email.errorMessage.value"
-        type="email"
-        label="Email"
-        append-inner-icon="mdi-email"
-        required
-      ></v-text-field>
-      <v-text-field 
-        class="mb-1"
-        v-model="password.value.value"
-        :type="showPassword?'text':'password'"
-        label="Create Password"
-        :error-messages="password.errorMessage.value"
-        :append-inner-icon="showPassword?'mdi-eye':'mdi-eye-off'"
-        @click:append-inner="showHidePassword('password')"
-        required
-      ></v-text-field>
-      <v-text-field
-        class="mb-1"
-        v-model="confirmPassword.value.value"
-        :type="showConfirmPassword?'text':'password'"
-        label="Confirm password"
-        :error-messages="confirmPassword.errorMessage.value" 
-        :append-inner-icon="showConfirmPassword?'mdi-eye':'mdi-eye-off'"
-        @click:append-inner="showHidePassword('confirmPassword')"
-        required
-      ></v-text-field>
-      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-    </v-form>
-
+    </v-card-title>
+    <v-divider class="mb-5"></v-divider>
+    <v-card-text>
+      <v-form ref="form" novalidate @submit.prevent="onFirstSubmit" :class="showFirstForm?'d-block':'d-none'">
+        <v-text-field
+          class="mb-1"
+          v-model="username.value.value"
+          :error-messages="username.errorMessage.value"
+          type="text"
+          label="Username"
+          append-inner-icon="mdi-account"
+          required
+        ></v-text-field>
+        <v-text-field
+          class="mb-1"
+          v-model="email.value.value"
+          :error-messages="email.errorMessage.value"
+          type="email"
+          label="Email"
+          append-inner-icon="mdi-email"
+          required
+        ></v-text-field>
+        <v-text-field 
+          class="mb-1"
+          v-model="password.value.value"
+          :type="showPassword?'text':'password'"
+          label="Create Password"
+          :error-messages="password.errorMessage.value"
+          :append-inner-icon="showPassword?'mdi-eye':'mdi-eye-off'"
+          @click:append-inner="showHidePassword('password')"
+          required
+        ></v-text-field>
+        <v-text-field
+          class="mb-1"
+          v-model="confirmPassword.value.value"
+          :type="showConfirmPassword?'text':'password'"
+          label="Confirm password"
+          :error-messages="confirmPassword.errorMessage.value" 
+          :append-inner-icon="showConfirmPassword?'mdi-eye':'mdi-eye-off'"
+          @click:append-inner="showHidePassword('confirmPassword')"
+          required
+        ></v-text-field>
+        <v-btn class="mt-2" type="submit" block>Next</v-btn>
+      </v-form>
+      
+      <second-form-register :class="showSecondForm?'d-block':'d-none'"></second-form-register>
+    </v-card-text>
   </v-card>
 </template>
 
 <script setup>
+
+  import SecondFormRegister from "../components/SecondFormRegister.vue"
+
   import { useField, useForm } from 'vee-validate'
   import { ref } from 'vue'
   import * as Yup from 'yup';
 
+
   let showPassword = ref(false);
   let showConfirmPassword = ref(false);
+
+  let showFirstForm = ref(true);
+  let showSecondForm = ref(true);
 
 
   function showHidePassword(type){
@@ -81,8 +92,10 @@
   const password = useField('password')
   const confirmPassword = useField('confirmPassword')
 
-  const onSubmit = handleSubmit(values=>{
+  const onFirstSubmit = handleSubmit(values=>{
       alert(JSON.stringify(values, null, 2))
+      showFirstForm.value = false;
+      showSecondForm.value = true;
   })
 
 
