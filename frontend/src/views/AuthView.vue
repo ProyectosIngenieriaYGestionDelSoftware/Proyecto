f81aa8785a46debcd0736a5266e05d840f11f818
jpere <jpereiro1@gmail.com>
2024-04-18 08:54:46 +0100

    Authentication restructuring

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,28 +1,38 @@
 <template>
   <v-card>
     <v-tabs
-      v-model=""
+      v-model="tab"
       bg-color="primary"
     >
-      <v-tab value="one">Item One</v-tab>
-      <v-tab value="two">Item Two</v-tab>
-      <v-tab value="three">Item Three</v-tab>
+      <v-tab value="register">Register</v-tab>
+      <v-tab value="login">LogIn</v-tab>
     </v-tabs>
 
     <v-card-text>
-      <v-window v-model="">
-        <v-window-item value="one">
-          One
+      <v-window v-model="tab">
+        <v-window-item value="register">
+          <register></register>
         </v-window-item>
 
-        <v-window-item value="two">
-          Two
+        <v-window-item value="login">
+          <login></login>
         </v-window-item>
 
-        <v-window-item value="three">
-          Three
-        </v-window-item>
       </v-window>
     </v-card-text>
   </v-card>
 </template>
+
+<script>
+  import Register from "../components/Register.vue";
+  import Login from "../components/Login.vue";
+  export default {
+    components:{
+      Register,
+      Login
+    },
+    data: () => ({
+      tab: null,
+    }),
+  }
+</script>
